c08522a11218708ae9e08eb2056259810a95b74c
snksergio <sergio.nyuxd@gmail.com>
2022-01-05 13:35:12 -0300

    Create designtokens.js

---
+++ designtokens.js
@@ -0,0 +1,99 @@
+{
+  "Foundations": {
+    "Primary": {
+      "Darker": {
+        "value": "#013080",
+        "type": "color"
+      },
+      "Dark": {
+        "value": "#0043a8",
+        "type": "color"
+      },
+      "Regular": {
+        "value": "#025EFE",
+        "type": "color"
+      },
+      "Medium": {
+        "value": "#4085fd",
+        "type": "color"
+      },
+      "Light": {
+        "value": "#afccff",
+        "type": "color"
+      },
+      "Lighter": {
+        "value": "#f0f5ff",
+        "type": "color"
+      }
+    },
+    "Neutral": {
+      "Darker": {
+        "value": "#010101",
+        "type": "color"
+      },
+      "Dark": {
+        "value": "#596579",
+        "type": "color"
+      },
+      "Regular": {
+        "value": "#7b8698",
+        "type": "color"
+      },
+      "Medium": {
+        "value": "#d0d7e2",
+        "type": "color"
+      },
+      "Light": {
+        "value": "#e6eaf0",
+        "type": "color"
+      },
+      "Lighter": {
+        "value": "#f7f9fc",
+        "type": "color"
+      },
+      "Bright": {
+        "value": "#ffffff",
+        "type": "color"
+      }
+    },
+    "Alias": {
+     "buttons": {
+      "primary": {
+        "backgroundColor": {
+          "value": "{Primary.Regular}",
+          "type": "color",
+          "description": ""
+        },
+        "color": {
+          "value": "{Neutral.Bright}",
+          "type": "color"
+        }
+      },
+      "Default": {
+        "backgroundColor": {
+          "value": "{Primary.Lighter}",
+          "type": "color"
+        },
+        "color": {
+          "value": "{Primary.Regular}",
+          "type": "color"
+        }
+      },
+      "Ghost": {
+        "backgroundColor": {
+          "value": "{Neutral.Bright}",
+          "type": "color"
+        },
+        "color": {
+          "value": "{Primary.Regular}",
+          "type": "color"
+        },
+        "borderColor": {
+          "value": "{Primary.Regular}",
+          "type": "color"
+        }
+      }
+    }
+  }
+    }
+}
